916b8b1bb5c2532c89572cca668f8aaf8a37bc07
MSSohan <sohan.cu.eee.17@gmail.com>
2024-10-29 05:26:50 +0600

    running one script at a time

--- index.php
+++ index.php
@@ -5,14 +5,12 @@ require __DIR__ . '/vendor/autoload.php'; // Adjust the path if necessary
 use Mqtt\MqttPhp\Controller\MqttServer;
 use Mqtt\MqttPhp\Service\WebPageDesign;
 
-// Create an instance of MqttServer
+// Create an instance of the MQTT server
 $mqttServer = new MqttServer();
 
-// Start the MQTT server in a separate thread or process if needed
-$mqttServer->start(); // Uncomment this if you need to start the server here
+// Start the MQTT server
+// $mqttServer->start();
 
-// Create an instance of WebPageDesign
-$page = new WebPageDesign();
-
-// Render the webpage
-$page->render();
+// Serve the webpage
+$webPage = new WebPageDesign();
+$webPage->render();
